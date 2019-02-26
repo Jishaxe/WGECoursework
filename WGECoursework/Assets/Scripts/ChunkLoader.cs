27176364155a96a4e9e00d6ad24c3430ca5119d1
Jishaxe <josh@joshbe.me>
2019-02-26 13:02:26 +0000

    initial commit

---
+++ WGECoursework/Assets/Scripts/ChunkLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class ChunkLoader : MonoBehaviour
+{
+    // 
+    public void LoadChunk(string fileName)
+    {
+        XmlReader reader = XmlReader.Create(fileName);
+
+    }
+
+    private void Start()
+    {
+        LoadChunk("AssessmentChunk1.xml");
+    }
+}
