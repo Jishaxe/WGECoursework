7bee3790ec068db7d00a506ea0deab83e9e61535
Jishaxe <josh@joshbe.me>
2019-03-16 21:06:39 +0000

    hotbar improvements

---
+++ WGECoursework/Assets/Scripts/InventoryItemScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemScript : MonoBehaviour
+{
+    public Text numberText;
+    public RawImage image;
+    public Texture2D tileTexture;
+
+    public Block blockType;
+    public int amount = 0;
+
+    public void Start()
+    {
+        UpdateItem(Block.DIRT, 0);
+    }
+
+    public void UpdateItem(Block blockType, int amount)
+    {
+        this.amount = amount;
+        this.blockType = blockType;
+
+        if (amount == 0)
+        {
+            numberText.enabled = false;
+            image.enabled = false;
+        } else
+        {
+            numberText.enabled = true;
+            numberText.text = amount.ToString();
+            image.enabled = true;
+
+
+
+
+            int x, y, width, height;
+
+            Vector2 uvCoords = VoxelGenerator.Block2UV(blockType);
+
+            x = (int)(uvCoords.x * tileTexture.width);
+            y = (int)(uvCoords.y * tileTexture.height);
+            width = (int)(tileTexture.width * 0.5f);
+            height = (int)(tileTexture.height * 0.5f);
+
+            Texture2D newTex = new Texture2D(width, height);
+
+            Color[] pixels = tileTexture.GetPixels(x, y, width, height);
+            newTex.SetPixels(pixels);
+            newTex.Apply();
+
+            image.texture = newTex;
+        }
+    }
+}
---
+++ WGECoursework/Assets/Scripts/InventoryScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct InventoryItem
+{
+    string name;
+    Block type;
+    Texture2D texture;
+}
+public class InventoryScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
