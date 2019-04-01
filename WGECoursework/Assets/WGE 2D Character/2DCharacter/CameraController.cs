a8ae2d464c0e52df43488a145227306fa4f07653
Jishaxe <josh@joshbe.me>
2019-04-01 23:51:38 +0100

    scene 2 and camera follow

---
+++ WGECoursework/Assets/WGE 2D Character/2DCharacter/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    float initialZ;
+    public float tightness = 0.25f;
+
+    void Start()
+    {
+        initialZ = this.transform.position.z;
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+        Vector3 lerped = Vector3.Lerp(this.transform.position, target.transform.position, tightness);
+        lerped.z = initialZ;
+        this.transform.position = lerped;
+    }
+}
